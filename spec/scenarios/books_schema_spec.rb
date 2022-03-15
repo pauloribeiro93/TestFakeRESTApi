1b5a5e28bd0d35dd2bcb5f52a90d3a14e8e31bc0
pauloribeiro93 <pauloribeiro93@hotmail.com>
2022-03-15 16:48:59 -0300

    Ajuste expect bockSchema

--- spec/scenarios/books_schema_spec.rb
+++ spec/scenarios/books_schema_spec.rb
@@ -2,7 +2,10 @@ describe "Schemas /FakeRestAPI" do
   context "Validar Schemas da FakeRestAPI" do
     it "Valida Schema da rota Books" do
       findBook = Books.new.findGetBook(Helpers::numRandom())
-      expect(findBook).to match_json_schema("books_schema")
+      
+      expect(JSON.parse(findBook.body)).to include('id','title','description','pageCount','excerpt','publishDate')
+      expect(findBook.body).to match_json_schema("books_schema")
+      
     end
   end
 end
